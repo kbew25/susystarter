f7e5294e180237b85fed9de8293a49f635b08aee
Kirsty Bewley <kirsty@burfieldcreative.co.uk>
2016-09-23 15:18:26 +0100

    Base files

--- README.md
+++ README.md
@@ -8,15 +8,16 @@ sudo gem install -n /usr/local/bin css_parser
 sudo gem install -n /usr/local/bin susy
 sudo gem install -n /usr/local/bin breakpoint
 </pre>
-3. Delete gulp directory if not using gulp
+3. Delete resources directory if not needed
 
 If using compass:
 - <pre>compass watch</pre>
 
 If using gulp:
-1. Install Node.js https://nodejs.org/en/
-2. <pre>sudo npm install gulp -g</pre>
-3. <pre>npm init</pre>
+- Install Node.js https://nodejs.org/en/
+- <pre>sudo npm install gulp -g</pre>
+- <pre>npm init</pre>
+
 (optional)
-4. Move gulp/gulpfile.js to root and delete gulp directory
-5. <pre>npm install gulp gulp-compass gulp-autoprefixer gulp-minify-css gulp-uglify gulp-rename gulp-concat gulp-notify gulp-livereload gulp-plumber --save-dev</pre>
+- Move resources/gulpfile.js to root and delete directory
+- <pre>npm install gulp gulp-compass gulp-autoprefixer gulp-minify-css gulp-uglify gulp-rename gulp-concat gulp-notify gulp-livereload gulp-plumber --save-dev</pre>
---
+++ resources/gulpfile.js
@@ -0,0 +1,63 @@
+//load plugins
+var gulp = require('gulp'),
+compass = require('gulp-compass'),
+autoprefixer = require('gulp-autoprefixer'),
+minifycss = require('gulp-minify-css'),
+uglify = require('gulp-uglify'),
+rename = require('gulp-rename'),
+concat = require('gulp-concat'),
+notify = require('gulp-notify'),
+livereload = require('gulp-livereload'),
+plumber = require('gulp-plumber'),
+path = require('path');
+
+//the title and icon that will be used for the Grunt notifications
+var notifyInfo = {
+  title:'Gulp',
+  icon: path.join(__dirname,'gulp.png')
+};
+
+//error notification settings for plumber
+var plumberErrorHandler = {
+  errorHandler: notify.onError({
+    title: notifyInfo.title,
+    icon: notifyInfo.icon,
+    message: "Error: <%= error.message %>"
+  })
+};
+
+//styles
+gulp.task('styles',function() {
+  returngulp.src(['sass/*.scss'])
+  .pipe(plumber(plumberErrorHandler))
+  .pipe(compass({
+    config_file:'config.rb',
+    css:'stylesheets',
+    sass:'sass',
+  }))
+  .pipe(autoprefixer('last 2 version','safari 5','ie 7','ie 8','ie 9','opera 12.1','ios 6','android 4'))
+  .pipe(gulp.dest('stylesheets'))
+  //.pipe(rename({ suffix: '.min' }))
+  //.pipe(minifycss())
+  //.pipe(gulp.dest('css'))
+  .pipe(livereload());
+});
+
+//scripts
+gulp.task('scripts',function() {
+  returngulp.src('js/*.js')
+  .pipe(plumber(plumberErrorHandler))
+  .pipe(concat('main.js'))
+  .pipe(gulp.dest('js'))
+  //.pipe(rename({ suffix: '.min' }))
+  //.pipe(uglify())
+  //.pipe(gulp.dest('js'))
+});
+
+//watch
+gulp.task('watch',['styles','scripts'],function(){
+  livereload.listen();
+  gulp.watch('sass/*.scss',['styles']);
+  gulp.watch('js/*.js',['scripts']);
+  gulp.watch('stylesheets/*.css',['styles']);
+});
